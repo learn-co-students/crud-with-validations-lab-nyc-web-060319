956d3cab3b73a2e529e26795103350fd440deeae
JakubowskiA <arieljakubowski@gmail.com>
2019-07-02 15:11:31 -0400

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,2 @@
+Song.create(title: "Elmo's World", released: true, release_year: 1990, artist_name: "Elmo", genre: "Children's")
+Song.create(title: "Banana Phone", released: true, release_year: 1990, artist_name: "Raffi", genre: "Children's")
